a9b2f9fb4b1d7dc57e834c64dd159e18d2f11478
helperstop <akhilnani14@gmail.com>
2021-09-12 23:21:41 +0530

    Move letter to end

--- AlgoExpert/MoveLetterToEnd.py
+++ AlgoExpert/MoveLetterToEnd.py
@@ -1,6 +1,12 @@
 def moveElementToEnd(array, toMove):
     l = len(array)-1
-    while l > -1:
-        
-        print(i)
-moveElementToEnd([1,2,3,4,5,2,2,2],2)
+    end = len(array)-1
+    initial = 0
+    while initial < end:
+        while initial < end and  array[end] == toMove:
+            end -= 1
+        if(array[initial] == toMove):
+            array[initial], array[end] = array[end], array[initial]
+        initial += 1
+    return array
+print(moveElementToEnd([1,2,3,4,5,2,2,2],2))
