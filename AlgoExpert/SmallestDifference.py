22fef163c96a19c077d877abe6f11e8e76785fe8
helperstop <akhilnani14@gmail.com>
2021-09-14 10:20:19 +0530

    SmallestDifference

--- AlgoExpert/SmallestDifference.py
+++ AlgoExpert/SmallestDifference.py
@@ -1,6 +1,5 @@
 #Brute Force
-import math
-def smallestDifference(arrayOne, arrayTwo):
+def smallestDifferenceBruteForce(arrayOne, arrayTwo):
     minPair = []
     minDistance = 1000000
     for i in arrayOne:
@@ -8,4 +7,32 @@ def smallestDifference(arrayOne, arrayTwo):
             if(abs(i - j) < minDistance):
                 minDistance = abs(i-j)
                 minPair = [i,j]
+    return minPair
+
+def smallestDifference(arrayOne, arrayTwo):
+    arrayOne.sort()
+    arrayTwo.sort()
+    l1 =  0
+    l2 = 0
+    minPair = []
+    minDistance = 1000000
+    currentDiff = 0
+    while(l1 < len(arrayOne) and l2 < len(arrayTwo)):
+        firstNum = arrayOne[l1]
+        secondNum = arrayTwo[l2]
+        diff = firstNum - secondNum
+        if(diff == 0):
+            minPair = [firstNum, secondNum]
+            return minPair
+        else:
+            if(firstNum > secondNum):
+                currentDiff = firstNum - secondNum
+                l2 += 1
+            else:
+                currentDiff = secondNum - firstNum
+                l1 += 1
+            if(minDistance > currentDiff):
+                minDistance = currentDiff
+                minPair = [firstNum, secondNum]
+
     return minPair
