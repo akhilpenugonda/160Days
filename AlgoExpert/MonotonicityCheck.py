fd404d11572f5526d28a059ac1078981d180d9a5
helperstop <akhilnani14@gmail.com>
2021-09-26 13:43:42 +0530

    MonotonicityCheck

---
+++ AlgoExpert/MonotonicityCheck.py
@@ -0,0 +1,10 @@
+def isMonotonic(array):
+		
+	isIncreasing = True
+	isDecreasing = True
+	for i in range(1,len(array)):
+		if(array[i] > array[i-1]):
+			isDecreasing = False
+		if(array[i] < array[i-1]):
+			isIncreasing = False
+	return isIncreasing or isDecreasing
