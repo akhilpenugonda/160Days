8ef2186a763045299f502ca8bff9f0fe9e98abdf
helperstop <akhilnani14@gmail.com>
2021-10-01 23:27:19 +0530

    Inverted binary tree practice

--- AlgoExpert/InvertBinaryTree.py
+++ AlgoExpert/InvertBinaryTree.py
@@ -11,7 +11,6 @@ def invert(tree):
 
 
 def invertBinaryTree(tree):
-    invert(tree)
     treeStack = [tree]
     while len(treeStack) > 0:
         item  = treeStack.pop()
@@ -25,6 +24,16 @@ def swapTreeElement(tree):
     tree.left, tree.right = tree.right, tree.left
 
 
+def invertBinaryTreePracticeWithWhile(tree):
+    treeStack = [tree]
+    while len(treeStack) > 0:
+        item = treeStack.pop()
+        if(item) is None:
+            continue
+        item.left, item.right = item.right, item.left
+        treeStack.append(item.left)
+        treeStack.append(item.right)
+    return tree
 class BinaryTree:
     def __init__(self, value):
         self.value = value
