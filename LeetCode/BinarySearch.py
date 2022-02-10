baf89280d07b313371c1b716b8d64b66cea3b20a
helperstop <akhilnani14@gmail.com>
2022-02-10 23:26:27 +0530

    Mademore changes

--- LeetCode/BinarySearch.py
+++ LeetCode/BinarySearch.py
@@ -1,14 +1,14 @@
-from ast import List
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        h = len(nums) -1
-        while (h >= l):
-            m = (l + h)//2
-            if(nums[m] == target):
-                return m
-            if(nums[m] > target):
-                h = m - 1
-            if(nums[m] < target):
-                l = m + 1
-        return -1
+def search(nums, target):
+    l = 0
+    h = len(nums) -1
+    while (h >= l):
+        m = (l + h)//2
+        if(nums[m] == target):
+            return m
+        if(nums[m] > target):
+            h = m - 1
+        if(nums[m] < target):
+            l = m + 1
+    return -1
+print(search([1,2,3],3))
+x = 1
