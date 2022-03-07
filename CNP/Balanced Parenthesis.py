68fdd7bfacfb05c1d236fbe4f855e22528a444d4
helperstop <akhilnani14@gmail.com>
2022-03-07 09:33:11 +0530

    stack using LL

---
+++ CNP/Balanced Parenthesis.py
@@ -0,0 +1,38 @@
+#{}()
+#{()}
+#{{{()}}}
+#{(}) X-Wrong
+#Example {(a=b) + (c + d) + e}
+#given only ( or ) exists
+
+from sys import stdin
+from StackWithLL import Stack
+def isBalanced(expression) :
+    stack = Stack()
+    for i in expression:
+        if(i in ["("]):
+            stack.push("(")   
+            print(i)
+            continue
+        elif(i in [")"]):
+            if(stack.isEmpty()):
+                return False
+            top = stack.pop()
+            if(top in ["("]):
+                continue
+            else:
+                return False
+    if(stack.isEmpty()):
+        return True
+    else: 
+        return False
+
+expression = stdin.readline().strip()
+
+if isBalanced(expression) :
+	print("true")
+
+else :
+	print("false")
+
+
--- CNP/StackWithLL.py
+++ CNP/StackWithLL.py
@@ -1,8 +1,7 @@
 from sys import stdin
 
-#Following is the structure of the node class for a Singly Linked List
 class Node :
-
+    
     def __init__(self, data) :
         self.data = data
         self.next = None
@@ -60,36 +59,3 @@ class Stack :
         #Implement the top() function
         
 
-
-
-
-#main
-q = int(stdin.readline().strip())
-
-stack = Stack()
-
-while q > 0 :
-
-    inputs = stdin.readline().strip().split(" ")
-    choice = int(inputs[0])
-
-    if choice == 1 :
-        data = int(inputs[1])
-        stack.push(data)
-
-    elif choice == 2 :
-        print(stack.pop())
-
-    elif choice == 3 :
-        print(stack.top())
-
-    elif choice == 4 : 
-        print(stack.getSize())
-
-    else :
-        if stack.isEmpty() :
-            print("true")
-        else :
-            print("false")
-
-    q -= 1
