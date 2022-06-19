7a209ef426441829f6d0b76bb92acb3bf9308778
helperstop <akhilnani14@gmail.com>
2022-06-19 13:52:55 +0530

    Added all mincost possibilities

--- CNP/MinCostPath.py
+++ CNP/MinCostPath.py
@@ -26,7 +26,30 @@ def minCostPath(cost, i, j, mRows, nCols, dp):
     dp[i][j] = cost[i][j] + min(ans1, ans2, ans3)
     return dp[i][j]
 
-
+def minCostIterative(cost, n, m):
+    dp = [[MAX_VALUE for j in range(m+1)] for i in range(n+1)]
+    for i in range(n-1, -1, -1):
+        for j in range(m-1,-1,-1):
+            if(i == n-1 and j == m-1):
+                dp[i][j] = cost[i][j]
+            else:
+                ans1 = dp[i+1][j]
+                ans2 = dp[i][j+1]
+                ans3 = dp[i+1][j+1]
+                dp[i][j] = cost[i][j] + min(ans1, ans2, ans3)
+    return dp[0][0]
+def minCostPathIterativeTD(cost, n,m):
+    dp = [[MAX_VALUE for i in range(m+1)] for j in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if(i == 1 and j ==1):
+                dp[i][j] = cost[0][0]
+            else:
+                ans1 = dp[i-1][j]
+                ans2 = dp[i][j-1]
+                ans3 = dp[i-1][j-1]
+                dp[i][j] = cost[i-1][j-1] + min(ans1, ans2, ans3)
+    return dp[n][m]
 #taking input using fast I/O method
 def take2DInput() :
     li = stdin.readline().rstrip().split(" ")
@@ -44,3 +67,5 @@ def take2DInput() :
 mat, mRows, nCols = take2DInput()
 dp = [[MAX_VALUE for j in range(nCols+1)] for i in range(mRows+1)]
 print(minCostPath(mat, 0, 0, mRows, nCols, dp))
+print(minCostIterative(mat, mRows, nCols))
+print(minCostPathIterativeTD(mat, mRows, nCols))
